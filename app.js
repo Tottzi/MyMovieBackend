48b506772404cf99b67b21229d3968969cb0017e
Tottzi <tottzi@gmail.com>
2021-07-11 15:25:38 +0200

    mongo URI

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || '5000'
 const {moviesApi, usersApi} = require('./db/client/api')
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+mongoose.connect('mongodb+srv://' + process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
